8f83553087fa051e45d3c7bb3ef054060ee6bdde
Parsifal1986 <parsifal1986@sjtu.edu.cn>
2024-07-29 10:17:52 +0800

    add a set function to the Alu

--- Simu/module/alu_module.hpp
+++ Simu/module/alu_module.hpp
@@ -4,28 +4,40 @@
 #include "tools.hpp"
 #include <cstdint>
 #include <iostream>
-#include <memory>
 #include <sys/types.h>
 
 namespace parsifal_modules {
 
 struct AluInput {
-  std::shared_ptr<Wire> a;
-  std::shared_ptr<Wire> b;
-  std::shared_ptr<Wire> operand;
+  shared_ptr<Wire> a;
+  shared_ptr<Wire> b;
+  shared_ptr<Wire> operand;
+
+  AluInput &operator=(const AluInput &other) {
+    a = other.a;
+    b = other.b;
+    operand = other.operand;
+    return *this;
+  }
 };
 
 struct AluOutput {
-  std::shared_ptr<Reg> result;
-  std::shared_ptr<Reg> done;
+  shared_ptr<Reg> result;
+  shared_ptr<Reg> done;
 
   void Update() {
     done->Update();
     result->Update();
   }
+
+  AluOutput &operator=(const AluOutput &other) {
+    result = other.result;
+    done = other.done;
+    return *this;
+  }
 };
 
-class Alu : public Module {
+class AluModule : public Module {
 private:
   enum AluOperand {
     ADD,
@@ -49,79 +61,88 @@ private:
   AluOutput output;
 
 public:
+  void Set(AluInput alu_input, AluOutput alu_output) {
+    input = alu_input;
+    output = alu_output;
+  }
+
   void Work() override {
-    switch (input.operand->Toi()) {
-      case ADD: {
-        (*output.result) <= input.a->Toi() + input.b->Toi();
-        break;
+    if ((*input.operand)[31] == 0) {
+      switch (input.operand->Toi()) {
+        case ADD: {
+          (*output.result) <= input.a->Toi() + input.b->Toi();
+          break;
+        }
+        case SUB: {
+          (*output.result) <= input.a->Toi() - input.b->Toi();
+          break;
+        }
+        case AND: {
+          (*output.result) <= (input.a->Toi() & input.b->Toi());
+          break;
+        }
+        case OR: {
+          (*output.result) <= (input.a->Toi() | input.b->Toi());
+          break;
+        }
+        case XOR: {
+          (*output.result) <= (input.a->Toi() ^ input.b->Toi());
+          break;
+        }
+        case SLL: {
+          (*output.result) <= (input.a->Toi() << input.b->Toi());
+          break;
+        }
+        case SRL: {
+          (*output.result) <= (input.a->Toi() >> input.b->Toi());
+          break;
+        }
+        case SRA: {
+          (*output.result) <= (uint32_t)(((int)input.a->Toi()) >> input.b->Toi());
+          break;
+        }
+        case SLT: {
+          (*output.result) <= ((int)input.a->Toi() < (int)input.b->Toi());
+          break;
+        }
+        case SLTU: {
+          (*output.result) <= (input.a->Toi() < input.b->Toi());
+          break;
+        }
+        case BEQ: {
+          (*output.result) <= (input.a->Toi() == input.b->Toi());
+          break;
+        }
+        case BNE: {
+          (*output.result) <= (input.a->Toi() != input.b->Toi());
+          break;
+        }
+        case BGE: {
+          (*output.result) <= ((int)input.a->Toi() >= (int)input.b->Toi());
+          break;
+        }
+        case BGEU: {
+          (*output.result) <= (input.a->Toi() >= input.b->Toi());
+          break;
+        }
+        case BLT: {
+          (*output.result) <= ((int)input.a->Toi() < (int)input.b->Toi());
+          break;
+        }
+        case BLTU: {
+          (*output.result) <= (input.a->Toi() < input.b->Toi());
+          break;
+        }
+        default: {
+          (*output.result) <= 0xffff;
+          std::cerr << "Wrong Opecode!" << std::endl;
+          break;
+        }
       }
-      case SUB: {
-        (*output.result) <= input.a->Toi() - input.b->Toi();
-        break;
-      }
-      case AND: {
-        (*output.result) <= (input.a->Toi() & input.b->Toi());
-        break;
-      }
-      case OR: {
-        (*output.result) <= (input.a->Toi() | input.b->Toi());
-        break;
-      }
-      case XOR: {
-        (*output.result) <= (input.a->Toi() ^ input.b->Toi());
-        break;
-      }
-      case SLL: {
-        (*output.result) <= (input.a->Toi() << input.b->Toi());
-        break;
-      }
-      case SRL: {
-        (*output.result) <= (input.a->Toi() >> input.b->Toi());
-        break;
-      }
-      case SRA: {
-        (*output.result) <= (uint32_t)(((int)input.a->Toi()) >> input.b->Toi());
-        break;
-      }
-      case SLT: {
-        (*output.result) <= ((int)input.a->Toi() < (int)input.b->Toi());
-        break;
-      }
-      case SLTU: {
-        (*output.result) <= (input.a->Toi() < input.b->Toi());
-        break;
-      }
-      case BEQ: {
-        (*output.result) <= (input.a->Toi() == input.b->Toi());
-        break;
-      }
-      case BNE: {
-        (*output.result) <= (input.a->Toi() != input.b->Toi());
-        break;
-      }
-      case BGE: {
-        (*output.result) <= ((int)input.a->Toi() >= (int)input.b->Toi());
-        break;
-      }
-      case BGEU: {
-        (*output.result) <= (input.a->Toi() >= input.b->Toi());
-        break;
-      }
-      case BLT: {
-        (*output.result) <= ((int)input.a->Toi() < (int)input.b->Toi());
-        break;
-      }
-      case BLTU: {
-        (*output.result) <= (input.a->Toi() < input.b->Toi());
-        break;
-      }
-      default: {
-        (*output.result) <= 0xffff;
-        std::cerr << "Wrong Opecode!" << std::endl;
-        break;
-      }
+      (*output.done) <= 1;
+    } else {
+      (*output.done) <= 0;
     }
-    (*output.done) <= 1;
   }
 
   void Update() override {
