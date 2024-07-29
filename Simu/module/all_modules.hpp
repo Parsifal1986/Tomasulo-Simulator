566f509452e36bf8de58c9bcdc95bee306cd5d4a
Parsifal1986 <parsifal1986@sjtu.edu.cn>
2024-07-29 10:17:04 +0800

    include all the modules that I've written

--- Simu/module/all_modules.hpp
+++ Simu/module/all_modules.hpp
@@ -1,11 +1,23 @@
 #ifndef ALL_MODULES_HPP
 #define ALL_MODULES_HPP
 
+#include "BP_module.hpp"
+#include "RS_module.hpp"
 #include "alu_module.hpp"
+#include "instruction_module.hpp"
 #include "memory_module.hpp"
+#include "ROB_module.hpp"
 
-using parsifal_modules::Alu;
+using parsifal_modules::AluModule;
 
 using parsifal_modules::MemoryModule;
+
+using parsifal_modules::RobModule;
+
+using parsifal_modules::BpModule;
+
+using parsifal_modules::RsModule;
+
+using parsifal_modules::InstructionModule;
 
 #endif // ALL_MODULES_HPP
