5876fa1d68a5850720ebff2f9c64b8e1ef807682
Parsifal1986 <parsifal1986@sjtu.edu.cn>
2024-07-29 10:15:46 +0800

    include all the tools to the tools header file

--- Simu/include/tools.hpp
+++ Simu/include/tools.hpp
@@ -1,10 +1,14 @@
 #ifndef  TOOLS_HPP
 #define TOOLS_HPP
 
+#include "cdb.hpp"
 #include "cpu.hpp"
 #include "module.hpp"
 #include "register.hpp"
 #include "wire.hpp"
+#include "queue.hpp"
+#include "register_file.hpp"
+#include <memory>
 
 using parsifal_tools::Wire;
 
@@ -13,5 +17,13 @@ using parsifal_tools::Reg;
 using parsifal_tools::Module;
 
 using parsifal_tools::Cpu;
+
+using parsifal_tools::Queue;
+
+using parsifal_tools::CDB;
+
+using parsifal_tools::RegisterFile;
+
+using std::shared_ptr;
 
 #endif // TOOLS_HPP
